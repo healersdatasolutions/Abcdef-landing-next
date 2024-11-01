bf8ba331f23fdda710760df959af9449d94e56a8
RuffledZest <vibhanshalok@gmail.com>
2024-11-01 22:43:27 +0530

    changes in the bento grid 1

--- src/components/ui/card-stack.tsx
+++ src/components/ui/card-stack.tsx
@@ -37,25 +37,48 @@ export const CardStack = ({
         newArray.unshift(newArray.pop()!); // move the last element to the front
         return newArray;
       });
-    }, 5000);
+    }, 3000);
+  };
+  const variants = {
+    initial: { backgroundPosition: "0 50%" },
+    animate: { backgroundPosition: ["0, 50%", "100% 50%", "0 50%"] },
   };
 
   return (
     <div className="relative mt-10 w-full h-full">
       {cards.map((card, index) => {
         return (
+          
+
           <motion.div
             key={card.id}
-            className="absolute dark:bg-[#0c1727]  h-32 w-full  rounded-3xl p-4 shadow-xl border border-neutral-200 dark:border-white/[0.1]  shadow-black/[0.1] dark:shadow-white/[0.05] flex flex-col justify-evenly"
+            variants={variants}
+            
+            className="absolute   h-36 w-full  rounded-xl  shadow-xl border border-black dark:border-black  shadow-black/[0.1] dark:shadow-black "
             style={{
               transformOrigin: "top center",
+              
+        backgroundSize: "400% 400%",
             }}
             animate={{
               top: index * -CARD_OFFSET,
               scale: 1 - index * SCALE_FACTOR, // decrease scale for cards that are behind
               zIndex: cards.length - index, //  decrease z-index for the cards that are behind
+              backgroundPosition: ["0, 50%", "100% 50%", "0 50%"],
             }}
           >
+            <motion.div
+      initial="initial"
+      animate="animate"
+      className="  h-full w-full p-4  rounded-xl shadow-xl border   shadow-black/[0.1] dark:shadow-black flex flex-col justify-evenly"
+
+      variants={variants}
+      transition={{ duration: 5, repeat: Infinity, repeatType: "reverse" }}
+      style={{
+        background: "linear-gradient(-45deg, #0f243d, #040e3d, #23a6d5, #23d5ab)",
+        backgroundSize: "400% 400%",
+      }}>
+
             <div className="font-normal text-neutral-700 dark:text-neutral-200">
               <p className="text-neutral-500 font-medium dark:text-white">
                 {card.name}
@@ -67,7 +90,10 @@ export const CardStack = ({
                 {card.designation}
               </p>
             </div>
+      </motion.div>
           </motion.div>
+     
+          
         );
       })}
     </div>
